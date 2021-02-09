9aafa25ae98b646511f63a84c7ba10855114c0ed
Yessenia <vela.kathy@gmail.com>
2021-02-09 15:58:33 -0500

    visualización de la data de los deportistas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import copyAthletes from './data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(example,copyAthletes);
+
+const dataAthletes = (copyAthletes.athletes);
+const contenedor = document.getElementById ("contenedor");
+
+const data = Object.values(dataAthletes);
+const showData = (parametro) => { 
+    let show ='';
+    parametro.forEach((element) => {
+        const deportistas = `
+        <div class="card">
+
+            <h1>
+                ${element.name}
+            </h1>
+            <p> ${element.noc}</p>
+        </div>
+        `;
+        show += deportistas;
+    });
+    contenedor.innerHTML = show;
+};
+showData(data);
