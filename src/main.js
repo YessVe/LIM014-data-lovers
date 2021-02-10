88324e0dabb5285eefdaaaf46317afdd65b998a8
nangebav <nangebav@gmail.com>
2021-02-10 17:33:24 -0500

    reconociendo cambios

--- src/main.js
+++ src/main.js
@@ -3,27 +3,32 @@ import { example } from './data.js';
 import copyAthletes from './data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+
 console.log(example,copyAthletes);
 
 const dataAthletes = (copyAthletes.athletes);
 const contenedor = document.getElementById ("contenedor");
 
+
+
 const data = Object.values(dataAthletes);
 const showData = (parametro) => { 
     let show ='';
+    
     parametro.forEach((element) => {
+        //Plantillas literales (plantillas de cadenas)=>${}//
         const deportistas =
-
         `<div class="card">
-            <div> </div>
+        <img src = ${element.gender === 'F'
+        //Operador condicional (ternario)//
+        ? './images/chica_uno.png'
+        : './images/chico_uno.png'}
             <h1>
-                ${element.name}
-            </h1>
-            
+            ${element.name}
+            </h1>    
             <p> ${element.noc}</p>
         </div>
         `;
-    
 
         show += deportistas;
     });
