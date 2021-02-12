ec600d4870034e2cff33c231ac50bf100d908515
Yessenia <vela.kathy@gmail.com>
2021-02-12 11:31:48 -0500

    escoger aleatoriamente la imagen de chico o chica de la card

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //Importaré la data
 import { example } from './data.js';
+import athletes from './data/athletes/athletes.js';
 
 import copyAthletes from './data/athletes/athletes.js';
 
@@ -11,13 +12,15 @@ console.log(example,copyAthletes);
 //está la info, la propiedad solo de 'athletes'
 const dataAthletes = (copyAthletes.athletes);
 
-//const data = Object.values(dataAthletes);
-
 //esta var me va a reconocer todos los valores
 const data = Object.values(dataAthletes);
 
+//array para las imágenes de femenino o masculino
+let avatarHombre = ["./images/card_boy_1.png", "./images/card_boy_2.png"];
+let avatarMujer = ["./images/card_girl_1.png","./images/card_girl_2.png"];
+
 //Creando una función para mostrar la data
-//Parametro de lo que se va a jalar de lo que contiene athletes
+//Parametro de lo que se va a jalar de lo que contiene athletes... es mi condición
 const mostrarData = (parametro) => {
     //variable vacia para el contenedor de los cards
     let mostrar ='';
@@ -26,23 +29,22 @@ const mostrarData = (parametro) => {
         
         //Variable del contenido de cada card de cada deportista
         const deportista =
-        
         //Quiero mostrar cada objeto dentro del card
         //o Template string o Plantillas literales (plantillas de cadenas)" `${}` " 
         //string o cadenas de Texto (``) 
-        
         //Inserto ${ } al h1 y a los img para jalar el valor del 
-        //nombre, gender y noc, según corresponda a la condicional.
-
+        //nombre, gender y noc, según corresponda a la condicional
         `<div class="card">
         <h1>
         ${element.name}
         </h1>
         <section class="">
         <img class="avatar" src = ${element.gender === 'F'
-        //Operador condicional (ternario)//
-        ? './images/chica_dos.png'
-        : './images/chico_uno.png'}>
+        //Operador condicional (ternario) con uso de aleatorio para que la tarjeta de
+        //hombre o mujer cambie entre las dos opciones
+        
+        ? ''+avatarMujer[Math.abs(Math.round((Math.random() * avatarMujer.length-2)+1))]+''
+        : ''+avatarHombre[Math.abs(Math.round((Math.random() * avatarHombre.length-2)+1))]+''}>
         <section>
         
         <img width= 54 height= 30.9
@@ -57,27 +59,5 @@ const mostrarData = (parametro) => {
         //Todo se colocará dentro del contenedor/
     contenedor.innerHTML = mostrar;
 };
-mostrarData(data);
+mostrarData(data); 
 
-
-
-/* //experimento de YEss
-const contenedor = document.getElementById ("contenedor");
-let mostrar ="";
-    // para
-    for (let index = 0; index < 3; index++) {
-    const deportista = 
-    //para que pueda dibujar en el HTML y el stilo 
-    //lo defino con la clase "card"
-        `<div class="card">
-        <h1>`
-        ${element.name};
-        `</h1>
-        </div>
-        `;
-         mostrar = mostrar + deportista;
-         
-         contenedor.innerHTML = mostrar;
-    };
- */
- 
