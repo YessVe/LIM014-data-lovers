cd67cd7b9174b7ea798466f40f979cbdbca22f23
nangebav <nangebav@gmail.com>
2021-02-15 10:45:11 -0500

    cambios para las cards

--- src/main.js
+++ src/main.js
@@ -4,63 +4,66 @@
 import copyAthletes from './data/athletes/athletes.js';
 
 
-debugger;
-console.log(example,copyAthletes);
+/*debugger;
+connst buscador = document.getElementById ("search");
+const btnBuscar= document.getElementById ('btnBuscar'); 
+console.log(example,copyAthletes);/*
 
 // creo la variable que va a llamar desde el archivo donde
 //está la info, la propiedad solo de 'athletes'
-const dataAthletes = (copyAthletes.athletes);
-const contenedor = document.getElementById ("contenedor");
 
-/* const dataExm= [1,2,3,4,5,2,3,4,6,1,10]
-const filtrarDataAthletes =dataExm.filter((item,index) => {
+
+ const dataExm= [1,2,3,4,5,2,3,4,6,1,10]
+/*const filtrarDataAthletes =dataExm.filter((item,index) => {
     return dataExm.indexOf(item) === index
 })
 console.log (filtrarDataAthletes); */
 
+const dataAthletes = (copyAthletes.athletes);
+const contenedor = document.getElementById ("contenedor");
 //esta var me va a reconocer todos los valores
 const data = Object.values(dataAthletes);
 
-<<<<<<< HEAD
 //array para las imágenes de femenino o masculino
-let avatarHombre = ["./images/card_boy_1.png", "./images/card_boy_2.png", "./images/card_boy_3.png"];
-let avatarMujer = ["./images/card_girl_1.png","./images/card_girl_2.png","./images/card_girl_3.png","./images/card_girl_4.png"];
-=======
-let avatarHombre = ["./images/card_boy_1.png", "./images/card_boy_2.png"];
-let avatarMujer = ["./images/card_girl_1.png","./images/card_girl_2.png"];
->>>>>>> 1aabb5d77f8ad47785b0ff2f1995c92af32b115d
+let avatarHombre = ["./images/card_M_1.png", "./images/card_M_2.png", "./images/card_M_3.png"];
+let avatarMujer = ["./images/card_F_1.png","./images/card_F_2.png","../images/card_F_3.png","./images/card_F_4.png"];
 
 //FUNCIÓN PARA LA VISUALIZACIÓN DE CARDS
 const mostrarData = (parametro) => {
-    //Parametro de lo que se va a jalar de lo que contiene athletes... es mi condición
     let mostrar ='';
-    //Quiero mostrar cada objeto de la siguiente manera
     parametro.forEach((element) => {
-        //Variable del contenido de cada card de cada deportista
         const deportista =
         `   <div class=wrap>
             <div class=card-wrap>
                 <div class="card">
-                <section class="contraCara"></section>
                 <section class="cara">
+                    <div class="caraNombre">
                     <h1>
-                    ${element.name}
+                    ${element.name.toUpperCase()}
                     </h1>
-                    <img class="bandera" 
-                    src="https://restcountries.eu/data/${element.noc.toLowerCase()}.svg" alt="">
+                    </div>
                     <section>
                     <img class="avatar" src = ${element.gender === 'F'
-<<<<<<< HEAD
                     //Operador condicional (ternario)//
                     ? ''+avatarMujer[Math.abs(Math.round((Math.random() * avatarMujer.length-4)+1))]+''
-                    : ''+avatarHombre[Math.abs(Math.round((Math.random() * avatarHombre.length-3)+1))]+''} style="height: 17.5em; width: 13.5em;">
-                    </section>  
-=======
-                    ? ''+avatarMujer[Math.abs(Math.round((Math.random() * avatarMujer.length-2)+1))]+''
-                    : ''+avatarHombre[Math.abs(Math.round((Math.random() * avatarHombre.length-2)+1))]+''} style="height: 18.2em; width: 13.5em;">      
-                    <img class="bandera" width= 54 height= 30.9
+                    : ''+avatarHombre[Math.abs(Math.round((Math.random() * avatarHombre.length-3)+1))]+''} width= 180px>
+                    <img class="bandera" 
                     src="https://restcountries.eu/data/${element.noc.toLowerCase()}.svg" alt="">
->>>>>>> 1aabb5d77f8ad47785b0ff2f1995c92af32b115d
+                    </section>
+                </section>
+
+                <section class="contraCara">
+                    <section class="contraCaraContent">
+                        <p>
+                        Nombre: ${element.name}
+                        </p>
+                        <p>
+                        Género: ${element.gender}
+                        </p>
+                        <p>
+                        País: ${element.team}
+                        </p>
+                    <section>
                 </section>
                 </div>
             </div>
@@ -73,15 +76,28 @@ const mostrarData = (parametro) => {
 
 mostrarData(data); 
 
-//
-const btnBuscar= document.getElementById ('btnBuscar'); 
+/*
 btnBuscar.addEventListener('click', function() {
     const showFilter = filterItems(document.getElementById("search"));
    
 });
 
+const search = document.getElementById('search');
 function filterItems(query) {
     return dataAthletes.filter(function(el) {
         return el.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
     })
-  };
+
+const limpiarContenedor = () => {
+  contenedor.innerHTML = '';
+  return false;
+};
+btnBuscar.addEventListener('keyup', (event) => {
+  const term = event.target.value.toLowerCase();
+  const filterChampions = namFilt(data, term);
+  cleanContainer();
+  showData(filterChampions);
+});
+
+  }; */
+  
