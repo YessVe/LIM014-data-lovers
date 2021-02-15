008faabbaa049da9c0e5dc9c9c1ab31d9913c49d
Yessenia <vela.kathy@gmail.com>
2021-02-15 10:49:43 -0500

    función de buscar por nombre de atleta

--- src/main.js
+++ src/main.js
@@ -1,16 +1,70 @@
 //Importaré la data
-//import { example } from './data.js';
+import { example } from './data.js';
 //import athletes from './data/athletes/athletes.js';
 import copyAthletes from './data/athletes/athletes.js';
-
-
-debugger;
-console.log(example,copyAthletes);
 
 // creo la variable que va a llamar desde el archivo donde
 //está la info, la propiedad solo de 'athletes'
 const dataAthletes = (copyAthletes.athletes);
 const contenedor = document.getElementById ("contenedor");
+
+fnCargaGeneral(dataAthletes);
+
+//FUNCIÓN DE CARGA GENERAL DE TODOS LAS CARDS DE ATLETAS
+function fnCargaGeneral(dataAthletes) {
+    //esta var me va a reconocer todos los valores
+    const data = Object.values(dataAthletes);
+    const mostrarData = (parametro) => {
+        //Parametro de lo que se va a jalar de lo que contiene athletes... es mi condición
+        let mostrar ='';
+        let avatarHombre = ["./images/card_boy_1.png", "./images/card_boy_2.png", "./images/card_boy_3.png"];
+        let avatarMujer = ["./images/card_girl_1.png","./images/card_girl_2.png","./images/card_girl_3.png","./images/card_girl_4.png"];
+        //Quiero mostrar cada objeto de la siguiente manera
+        parametro.forEach((element) => {
+            //Contenido de cada card de cada deportista
+            const deportista =
+            `   <div class=wrap>
+                <div class=card-wrap>
+                    <div class="card">
+                    <section class="contraCara"></section>
+                    <section class="cara">
+                        <h1>
+                        ${element.name}
+                        </h1>
+                        <img class="bandera" 
+                        src="https://restcountries.eu/data/${element.noc.toLowerCase()}.svg" alt="">
+                        <section>
+                        <img class="avatar" src = ${element.gender === 'F'
+                        //Operador condicional (ternario)//
+                        ? ''+avatarMujer[Math.abs(Math.round((Math.random() * avatarMujer.length-4)+1))]+''
+                        : ''+avatarHombre[Math.abs(Math.round((Math.random() * avatarHombre.length-3)+1))]+''} style="height: 17.5em; width: 13.5em;">
+                        </section>  
+                    </section>
+                    </div>
+                </div>
+                </div>
+            `;
+            mostrar = mostrar + deportista;
+        });
+        contenedor.innerHTML = mostrar;
+    };
+    mostrarData(data); 
+}
+
+//FUNCIÓN PARA BUSCAR POR NOMBRE DE ATLETA CON CLICK
+const btnBuscar= document.getElementById ('btnBuscar'); 
+btnBuscar.addEventListener('click', function() {
+    const nameAthlete = document.getElementById("search").value;
+    const showFilter = filterItems(nameAthlete);
+    function filterItems(query){
+        console.log(query)
+        return dataAthletes.filter(function(el) {
+            return el.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+        })
+      };
+    fnCargaGeneral(showFilter);
+});
+  
 
 /* const dataExm= [1,2,3,4,5,2,3,4,6,1,10]
 const filtrarDataAthletes =dataExm.filter((item,index) => {
@@ -18,70 +72,5 @@ const filtrarDataAthletes =dataExm.filter((item,index) => {
 })
 console.log (filtrarDataAthletes); */
 
-//esta var me va a reconocer todos los valores
-const data = Object.values(dataAthletes);
-
-<<<<<<< HEAD
-//array para las imágenes de femenino o masculino
-let avatarHombre = ["./images/card_boy_1.png", "./images/card_boy_2.png", "./images/card_boy_3.png"];
-let avatarMujer = ["./images/card_girl_1.png","./images/card_girl_2.png","./images/card_girl_3.png","./images/card_girl_4.png"];
-=======
-let avatarHombre = ["./images/card_boy_1.png", "./images/card_boy_2.png"];
-let avatarMujer = ["./images/card_girl_1.png","./images/card_girl_2.png"];
->>>>>>> 1aabb5d77f8ad47785b0ff2f1995c92af32b115d
-
-//FUNCIÓN PARA LA VISUALIZACIÓN DE CARDS
-const mostrarData = (parametro) => {
-    //Parametro de lo que se va a jalar de lo que contiene athletes... es mi condición
-    let mostrar ='';
-    //Quiero mostrar cada objeto de la siguiente manera
-    parametro.forEach((element) => {
-        //Variable del contenido de cada card de cada deportista
-        const deportista =
-        `   <div class=wrap>
-            <div class=card-wrap>
-                <div class="card">
-                <section class="contraCara"></section>
-                <section class="cara">
-                    <h1>
-                    ${element.name}
-                    </h1>
-                    <img class="bandera" 
-                    src="https://restcountries.eu/data/${element.noc.toLowerCase()}.svg" alt="">
-                    <section>
-                    <img class="avatar" src = ${element.gender === 'F'
-<<<<<<< HEAD
-                    //Operador condicional (ternario)//
-                    ? ''+avatarMujer[Math.abs(Math.round((Math.random() * avatarMujer.length-4)+1))]+''
-                    : ''+avatarHombre[Math.abs(Math.round((Math.random() * avatarHombre.length-3)+1))]+''} style="height: 17.5em; width: 13.5em;">
-                    </section>  
-=======
-                    ? ''+avatarMujer[Math.abs(Math.round((Math.random() * avatarMujer.length-2)+1))]+''
-                    : ''+avatarHombre[Math.abs(Math.round((Math.random() * avatarHombre.length-2)+1))]+''} style="height: 18.2em; width: 13.5em;">      
-                    <img class="bandera" width= 54 height= 30.9
-                    src="https://restcountries.eu/data/${element.noc.toLowerCase()}.svg" alt="">
->>>>>>> 1aabb5d77f8ad47785b0ff2f1995c92af32b115d
-                </section>
-                </div>
-            </div>
-            </div>
-        `;
-        mostrar += deportista;
-    });
-    contenedor.innerHTML = mostrar;
-};
-
-mostrarData(data); 
-
-//
-const btnBuscar= document.getElementById ('btnBuscar'); 
-btnBuscar.addEventListener('click', function() {
-    const showFilter = filterItems(document.getElementById("search"));
-   
-});
-
-function filterItems(query) {
-    return dataAthletes.filter(function(el) {
-        return el.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
-    })
-  };
+// usar un map para llenar un vacio que contenga solo los nombres
+// y ese map se encarga de quitar los duplicados
