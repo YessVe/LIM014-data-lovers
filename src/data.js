3adf71270bc5449e549587d7db119d52bd698734
Yessenia <vela.kathy@gmail.com>
2021-02-22 11:35:26 -0500

    4 funciones escritas con sus parametros

--- src/data.js
+++ src/data.js
@@ -1,12 +1,57 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const allNames = (data) => { // data = []
+  const todosLosNombres = [];
+  for (let i = 0; i < data.length; i++) {
+    todosLosNombres.push(data[i].name);
+  }
+  return todosLosNombres;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const uniqueNames = (dataNames) => {
+  const nombresUnicos = [];
+  for (let i = 0; i < dataNames.length; i++) {
+    let nombre = dataNames[i];
+    if (nombresUnicos.indexOf(nombre) < 0) {
+      nombresUnicos.push(nombre);
+    }
+  }
+  return nombresUnicos;
 };
 
+export const cleanData = (dataNames, data) => {
+  const dataLimpia = [];
+  for (let i = 0; i < dataNames.length; i++) {
+    let nombrePU = dataNames[i];
+    let todosEventos = [];
+    let todasMedallas = [];
 
+    for (let j = 0; j < data.length; j++) {
+      if (nombrePU == data[j].name) {
 
+        todosEventos.push(data[j].event);
+        todasMedallas.push(data[j].medal);
+        var datos = {
+          name: data[j].name,
+          gender: data[j].gender,
+          height: data[j].height,
+          weight: data[j].weight,
+          sport: data[j].sport,
+          team: data[j].team,
+          noc: data[j].noc,
+          age: data[j].age,
+          events: todosEventos,
+          medals: todasMedallas,
+        };
+      }
+    }
+    dataLimpia.push(datos);
+  }
+  return dataLimpia;
+}
+
+export const filterItems = (query, data) => {
+  return data.filter((el) => {
+    return el.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+  })
+};
