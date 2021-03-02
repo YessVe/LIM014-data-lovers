a8dfb2bcc347f1a55c3803499bd826ac1dfab87d
Yessenia <vela.kathy@gmail.com>
2021-03-02 15:21:00 -0500

    se quedó la función indexOf para el filtro de países

--- src/data.js
+++ src/data.js
@@ -11,9 +11,9 @@ export const allNames = (data) => { // data = []
 export const unique = (data) => {
   const valoresUnicos = [];
   for (let i = 0; i < data.length; i++) {
-    let valorAtletaPaisDeporte = data[i];
-    if (valoresUnicos.indexOf(valorAtletaPaisDeporte) < 0) {
-      valoresUnicos.push(valorAtletaPaisDeporte);
+    let valorAtletaDeporte = data[i];
+    if (valoresUnicos.indexOf(valorAtletaDeporte) < 0) {
+      valoresUnicos.push(valorAtletaDeporte);
     }
   }
   return valoresUnicos;
@@ -64,7 +64,6 @@ export const alphabetOrder = (first, condition) => {
   const result = first;
   /* Si la comparacion resulta verdadero tomara como  valor "1" para que vaya a la
     primera posición */
-  
     const compareName = (a, b) => {
       if (a.name.toLowerCase() > b.name.toLowerCase()) return 1; 
       if (a.name.toLowerCase() === b.name.toLowerCase()) return 0;
@@ -100,7 +99,7 @@ export const allCountries = (data) =>{
   return todosLosPaises;
 }
 
-//
+//FUNCIÓN PARA FILTRAR PAÍSES
 export const uniqueCountry = (data) => {
   const valoresUnicosPais = [];
   for (let i = 0; i < data.length; i++) {
@@ -108,18 +107,18 @@ export const uniqueCountry = (data) => {
     if (valoresUnicosPais.indexOf(valorPais) < 0) {
       valoresUnicosPais.push(valorPais);
     }
-  }
+  } 
   const valoresUnicosOrdenados= valoresUnicosPais.sort();
   const valorFinalPais = [];
-  for (let i = 0; i < valoresUnicosOrdenados.length-1; i++) {
-    let valorPrimero = valoresUnicosOrdenados[i];
-    let valorSegundo = valoresUnicosOrdenados[i+1];
-    if(valorSegundo.indexOf(valorPrimero)<0){
+  for (let i = 0; i < valoresUnicosOrdenados.length; i++) {
+    let valorPrimero = valoresUnicosOrdenados[i-1];
+    let valorSegundo = valoresUnicosOrdenados[i];
+    if(valorSegundo.indexOf(valorPrimero)<0){ 
       valorFinalPais.push(valorSegundo);
-    }
+    } 
   }
   return valorFinalPais;
-};
+ };
 
 
 //FUNCIÓN PARA SELECCIONAR PAÍS
@@ -144,7 +143,3 @@ export const filterSport = (select,data) =>{
     return el.sport.indexOf(select) > -1;})
 }
 
-
-  /* if (query==0 || query=="" || isNaN(query) || query==Number) {
-    throw TypeError("Escribe letras, por favor")
-  } */
