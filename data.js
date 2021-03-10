7acf06a4083ee51e3a504164f0f9e1d17fd8e96e
Yessenia <vela.kathy@gmail.com>
2021-03-10 11:32:27 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,189 @@
+//1ro obtengo los nombres de toda la data
+export const allNames = (data) => { // data = []
+  const todosLosNombres = [];
+  for (let i = 0; i < data.length; i++) {
+    todosLosNombres.push(data[i].name);
+  }
+  return todosLosNombres;
+};
+
+//2do voy a sacar los nombres sea del atleta, país, deporte sin que se repita
+export const unique = (data) => {
+  const valoresUnicos = [];
+  for (let i = 0; i < data.length; i++) {
+    let valorAtletaDeporte = data[i];
+    if (valoresUnicos.indexOf(valorAtletaDeporte) < 0) {
+      valoresUnicos.push(valorAtletaDeporte);
+    }
+  }
+  return valoresUnicos;
+};
+
+//3ro voy a crear la data para los nombres de atletas que son únicos
+export const cleanData = (dataNames, data) => {
+  const dataLimpia = [];
+  for (let i = 0; i < dataNames.length; i++) {
+    let nombrePU = dataNames[i];
+    let todosEventos = [];
+    let todasMedallas = [];
+
+    for (let j = 0; j < data.length; j++) {
+      if (nombrePU == data[j].name) {
+        todosEventos.push(data[j].event);
+        todasMedallas.push(data[j].medal);
+        var datos = {
+          name: data[j].name,
+          gender: data[j].gender,
+          height: data[j].height,
+          weight: data[j].weight,
+          sport: data[j].sport,
+          team: data[j].team,
+          noc: data[j].noc,
+          age: data[j].age,
+          events: todosEventos,
+          medals: todasMedallas,
+        };
+      }
+    }
+    dataLimpia.push(datos);
+  }
+  return dataLimpia;
+}
+
+//FUNCIÓN PARA BUSCAR POR NOMBRE DE ATLETA
+export const filterName = (input, data) => {
+  return data.filter((el) => {
+    return el.name.toLowerCase().indexOf(input.toLowerCase()) > -1;
+  })
+};
+
+//FUNCIÓN PARA ORDENAR ALFABÉTICAMENTE (A-Z / Z-A)
+export const alphabetOrder = (first, condition) => {
+  //*La variable me servirá para ordenar las primeras letras del nombre del atleta  
+  const result = first;
+  /* Si la comparacion resulta verdadero tomara como  valor "1" para que vaya a la
+    primera posición */
+    const compareName = (a, b) => {
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return 1; 
+      if (a.name.toLowerCase() === b.name.toLowerCase()) return 0;
+      return -1;
+    }
+    if (condition === 'a-z') {
+      result.sort(compareName);
+    }
+    const compareNameReverse = (a, b) => {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return 1; 
+      if (a.name.toLowerCase() === b.name.toLowerCase()) return 0;
+      return -1;
+    }  
+    if (condition === 'z-a') {
+      result.sort(compareNameReverse);
+    }
+    return result;
+}; 
+
+//FUNCIÓN PARA SELECCIONAR GÉNERO
+export const filterGender = (input, data) => {
+  return data.filter((el) => {
+    return el.gender.indexOf(input) > -1;
+  })
+};
+
+//FUNCIÓN PARA TENER LA LISTA DE PAÍSES
+export const allCountries = (data) =>{
+  const todosLosPaises = [];
+  for (let i = 0; i < data.length; i++) {
+    todosLosPaises.push(data[i].team);
+  }
+  return todosLosPaises;
+};
+
+//FUNCIÓN PARA FILTRAR PAÍSES
+export const uniqueCountry = (data) => {
+  const valoresUnicosPais = [];
+  for (let i = 0; i < data.length; i++) {
+    let valorPais = data[i];
+    if (valoresUnicosPais.indexOf(valorPais) < 0) {
+      valoresUnicosPais.push(valorPais);
+    }
+  } 
+  const valoresUnicosOrdenados= valoresUnicosPais.sort();
+  const valorFinalPais = [];
+  for (let i = 0; i < valoresUnicosOrdenados.length; i++) {
+    let valorPrimero = valoresUnicosOrdenados[i-1];
+    let valorSegundo = valoresUnicosOrdenados[i];
+    if(valorSegundo.indexOf(valorPrimero)<0){ 
+      valorFinalPais.push(valorSegundo);
+    } 
+  }
+  return valorFinalPais;
+ };
+
+
+//FUNCIÓN PARA SELECCIONAR PAÍS
+export const filterCountry = (input, data) => {
+  return data.filter((el) => {
+    return el.team.indexOf(input) > -1;
+  })
+};
+
+
+//FUNCIÓN PARA TENER LA LISTA DE DEPORTE
+export const allSport = (data) => { // data = []
+  const todosLosDeportes = [];
+  for (let i = 0; i < data.length; i++) {
+    todosLosDeportes.push(data[i].sport);
+  }
+  return todosLosDeportes;
+};
+
+//FUNCIÓN PARA SELECCIONAR DEPORTE
+export const filterSport = (select,data) =>{
+  return data.filter((el) => {
+    return el.sport.indexOf(select) > -1;})
+};
+
+//FUNCIÓN PARA CONTAR LA CANTIDAD DE MEDALLAS
+export const countMedals = (data) => {
+  let oroMedalla = 0;
+  let plataMedalla = 0;
+  let bronceMedalla = 0;
+  //el acumulador por LEY se declara fuera del loop
+  for (let i = 0; i < data.length; i++) {
+      let cantidadMedalla=data[i].medals; 
+      for (let j = 0; j < cantidadMedalla.length; j++) {    
+          /* if (cantidadMedalla[j]=="Gold") {
+              oroMedalla = oroMedalla+1;
+          } else if (cantidadMedalla[j]=="Silver") { 
+              plataMedalla = plataMedalla+1;
+          } else if (cantidadMedalla[j]=="Bronze") {
+              bronceMedalla = bronceMedalla+1;
+          } */
+
+          switch (cantidadMedalla[j]) {
+            case "Gold":
+              oroMedalla = oroMedalla+1;
+              break;
+            case "Silver":
+              plataMedalla = plataMedalla+1;
+              break;
+            case "Bronze":
+              bronceMedalla = bronceMedalla+1;
+              break;
+          }
+      }
+  }
+  return (oroMedalla.toString()+"-"+plataMedalla.toString()+"-"+bronceMedalla.toString());
+}
+
+//FUNCIÓN EDADES
+export const ageOrder = (data, condition) => {
+  //*La variable me servirá para ordenar las primeras letras del nombre del atleta  
+    const ordenarEdad = data.sort((a,b) => (a.age < b.age ? -1 : 1)) 
+    if (condition=="J") {
+      return ordenarEdad.slice(0,5);
+    } else if (condition=="L") {
+      return ordenarEdad.slice(ordenarEdad.length-5,ordenarEdad.length);
+    }
+}; 
+
---
+++ main.js
@@ -0,0 +1,199 @@
+//Importaré la data
+import {unique, allNames, cleanData,filterName,alphabetOrder,filterGender,
+    allCountries,uniqueCountry, filterCountry,allSport, filterSport,countMedals,
+    ageOrder} from './data.js';
+
+//import athletes from './data/athletes/athletes.js';
+import copyAthletes from './data/athletes/athletes.js';
+
+// creo la variable que va a llamar desde el archivo donde
+//está la info, la propiedad solo de 'athletes'
+const dataAthletes = (copyAthletes.athletes);
+
+const contarAtletas =document.querySelector("#contarAtletas");
+const contarMedallas =document.querySelector("#contarMedallas");
+const selectOrdenar = document.getElementById('ordenar');
+const contenedor = document.getElementById ("contenedor");
+
+//FUNCIÓN PARA EL BOTÓN LIMPIAR
+const btnReset = document.getElementById('limpiar');
+btnReset.addEventListener('click',()=>{
+    contarMedallas.innerHTML="";
+    fnCargaGeneral(dataLimpia);
+    document.getElementById('search').value="";
+    document.getElementById('ordenar').value="";//las tarjetas se quedan ordenadas
+    document.getElementsByName('gender').value="";//el input se queda marcado
+    selectPaises.value="";
+    selectDeporte.value="";
+    document.getElementsByName('calculo').value="";
+    document.getElementById("promedio").value="";
+})
+
+//FUNCIÓN PARA OBTENER ATLETAS SIN DUPLICAR
+    //1ro obtengo los nombres de toda la data
+    const todosLosNombres = allNames(dataAthletes);
+    //dataAthletes es el argumento y puede ir cambiando a dataPokemon por ejemplo allNames(dataPokemon)
+     //2do voy a sacar los nombres sin que se repita
+    const nombresUnicos= unique (todosLosNombres);
+    //3ro voy a crear la data para los nombres de atletas que son únicos
+    const dataLimpia= cleanData(nombresUnicos,dataAthletes)
+
+fnCargaGeneral(dataLimpia);
+
+//FUNCIÓN DE CARGA GENERAL DE TODOS LAS CARDS DE ATLETAS
+function fnCargaGeneral(dataLimpia) {
+        //esta var me va a reconocer todos los valores
+        const data = Object.values(dataLimpia);
+        const mostrarData = (parametro) => {
+        //Parametro de lo que se va a jalar de lo que contiene athletes... es mi condición
+        let mostrar ='';
+        let avatarHombre = ["./images/card_M_1.png", "./images/card_M_2.png", "./images/card_M_3.png"];
+        let avatarMujer = ["./images/card_F_1.png","./images/card_F_2.png","./images/card_F_3.png","./images/card_F_4.png"];
+        //Quiero mostrar cada objeto de la siguiente manera
+        //funciona similar al array reduce para quitar los duplicados - antes de devolver el array
+        parametro.forEach((element) => {
+            //Contenido de cada card de cada deportista
+            const deportista =
+            `   <div class=wrap>
+                    <div class=card-wrap>
+                        <div class="card">        
+                            <section class="cara">
+                                <div class="caraNombre">
+                                    <h1>
+                                    ${element.name.toUpperCase()}
+                                    </h1>
+                                </div>
+                                
+                                <section>
+                                    <img class="avatar" src = ${element.gender === 'F'
+                                    //Operador condicional (ternario)//
+                                    ? ''+avatarMujer[Math.abs(Math.round((Math.random() * avatarMujer.length-4)+1))]+''
+                                    : ''+avatarHombre[Math.abs(Math.round((Math.random() * avatarHombre.length-3)+1))]+''} width= 180px>
+                                    <img class="bandera" 
+                                    src="https://www.fiba.basketball/api/img/team/logoflag/0?sizeType=Medium&backgroundType=Light&patternType=default_medium&eventId=10628&iocCode=${element.noc}" alt="">
+                                </section>  
+                            </section><!--Fin de class "cara"-->
+                            <section class="contraCara">
+                            <section class=contraCaraContent>
+                                <center><p class="name">${element.name}</p></center>
+                                <p>Disciplina: ${element.sport}</p>
+                                <p>Evento: ${element.events} </p>
+                                <p>Medalla: ${element.medals} </p>
+                                <p>País: ${element.team}</p>    
+                                <p>Género: ${element.gender}</p>
+                                <p>Edad: ${element.age}</p>
+                                <p>Estatura: ${element.height}cm</p>
+                                <p>Peso: ${element.weight}kg </p>
+                            </section>
+                            </section>
+                        </div> <!--Fin class "card"-->
+                    </div>
+                </div>
+            `;
+        mostrar += deportista;
+    });
+    contenedor.innerHTML = mostrar;
+};
+mostrarData(data); 
+contarAtletas.innerHTML="Atletas: "+dataLimpia.length;
+}
+
+
+//FUNCIÓN PARA BUSCAR POR NOMBRE DE ATLETA CON CLICK
+const txtBuscar= document.getElementById ('search'); 
+txtBuscar.addEventListener('keyup', ()=> {
+    const nameAthlete = document.getElementById("search").value;
+    const showFilter = filterName(nameAthlete,dataLimpia);  
+    if (showFilter=="") {
+        contenedor.innerHTML="Atleta no encontrad@";   
+    } else {
+        fnCargaGeneral(showFilter);
+    }
+});
+
+selectOrdenar.addEventListener('change', () => {
+    const valueOrder = selectOrdenar.value;
+    const showOrder = alphabetOrder(dataLimpia,valueOrder);
+    fnCargaGeneral(showOrder)
+});
+
+//FUNCIÓN PARA BUSCAR POR GÉNERO
+const radioBtnGenero = document.getElementsByName('gender');
+for (let i = 0; i < radioBtnGenero.length; i++) {
+      radioBtnGenero[i].addEventListener('change', () => {  
+        const valueGender = radioBtnGenero[i].value;
+        const showGender = filterGender(valueGender,dataLimpia);
+        fnCargaGeneral(showGender); 
+        let showMedals = countMedals(showGender); 
+        let x= showMedals.split("-");//busca el guión y lo parte convirtiéndolo en un array - "es poderoso"
+        contarMedallas.innerHTML= "Medallas: "+"Oro 🥇: "+x[0]+", "+"Plata 🥈: "+x[1]+", "+"Bronce 🥉: "+x[2]+".";
+    });
+}
+
+//FUNCIÓN PARA FILTRAR PAÍSES
+const todosLosPaises =allCountries(dataAthletes);
+const paisesUnicos =uniqueCountry(todosLosPaises);
+const selectPaises = document.getElementById("paises");
+for(let i=0; i < paisesUnicos.length; i++){ 
+    let option = document.createElement("option"); //Creamos la opcion
+    option.innerHTML = paisesUnicos[i]; //Metemos el texto en la opción + su bandera - la bandera viene del link
+    option.setAttribute('value',paisesUnicos[i])
+    selectPaises.appendChild(option); //Metemos la opción en el select
+}
+selectPaises.addEventListener('change', () => {
+    const valueCountry = selectPaises.value; 
+    const showCountry = filterCountry(valueCountry,dataLimpia);
+    fnCargaGeneral(showCountry); 
+    
+    let showMedals = countMedals(showCountry);
+    let x= showMedals.split("-");
+    contarMedallas.innerHTML= "Medallas: "+"Oro 🥇: "+x[0]+", "+"Plata 🥈: "+x[1]+", "+"Bronce 🥉: "+x[2]+".";
+    
+}); 
+
+//FUNCIÓN PARA FILTRAR POR DEPORTES
+    //Creo el evento para cuando use el seleccionador de deportes
+    const todosLosDeportes = allSport(dataAthletes);
+    const deportesUnicos= unique(todosLosDeportes);
+    deportesUnicos.sort();
+    const selectDeporte = document.getElementById("deportes"); 
+    for(let i=0; i < deportesUnicos.length; i++){ 
+        let option = document.createElement("option");
+        option.innerHTML = deportesUnicos[i]; 
+        selectDeporte.appendChild(option);
+        option.setAttribute('value', deportesUnicos[i])
+    }
+        //Creo el evento para cuando use el seleccionador de deportes
+    selectDeporte.addEventListener('change', () => {
+        const valueSport = selectDeporte.value;
+        const showSport = filterSport(valueSport,dataLimpia);
+        fnCargaGeneral(showSport)
+    }); 
+    
+//FUNCIÓN PARA TENER LOS 5 ATLETAS MÁS JÓVENES Y LOS 5 MÁS LONGEVOS
+const radioBtnEdades = document.getElementsByName('calculo');
+for (let i = 0; i < radioBtnEdades.length; i++) {
+    radioBtnEdades[i].addEventListener('change', () => { 
+        const valueAge = radioBtnEdades[i].value;
+        const edades = ageOrder(dataLimpia, valueAge) 
+        contarMedallas.innerHTML="";
+        fnCargaGeneral(edades);
+    });    
+}
+
+//FUNCIÓN PARA TENER EL PROMEDIO
+const sumatoriaEdades = dataLimpia.reduce((acumulador, siguienteValor) => acumulador+siguienteValor.age, 0); 
+//Si no hay nada, regresamos un objeto con edad = 0. No hay necesidad de devolver el nombre, pues no es necesario */ 
+const promedioEdad = Math.round(sumatoriaEdades / dataLimpia.length); 
+let radioBtnPromedio = document.getElementById("promedio"); 
+radioBtnPromedio.addEventListener('change', () => { 
+/* contenedor.innerHTML= "La edad promedio de todos los atletas es "+promedioEdad;  */
+contenedor.innerHTML= 
+`<div class="mensajes">La edad promedio de los atletas es ${promedioEdad} años</div>
+<img src="./images/wallpaper 2.png">`;
+contarAtletas.innerHTML="";
+contarMedallas.innerHTML="";
+});
+
+
+
